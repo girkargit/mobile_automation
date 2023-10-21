3f33c7abd463ff45d8fb885f93ceb0f32c6f31f7
girkargit <abhilashgirkar97@gmail.com>
2023-10-21 15:51:08 +0530

    change

--- testcases/test_03dialerapp.py
+++ testcases/test_03dialerapp.py
@@ -1,7 +1,12 @@
 import time
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
+from appium.webdriver.appium_service import AppiumService
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+appium_service = AppiumService()
+appium_service.start()
 
 desired_caps = {
     'deviceName': '5200d42b8c7a863b',
@@ -10,10 +15,14 @@ desired_caps = {
     'chromedriverExecutable':"C:\\Program Files\\Appium Server GUI\\resources\\app\\node_modules\\appium\\node_modules\\appium-chromedriver\\chromedriver\\chromedriver.exe",
     'browserName': 'Chrome'
 }
+
 capabilities_options = UiAutomator2Options().load_capabilities(desired_caps)
 appium_server_url = "http://127.0.0.1:4723/wd/hub"
 driver = webdriver.Remote(appium_server_url, options=capabilities_options)
-driver.get("http://google.com")
+driver.get("http://wikipedia.org")
 print(driver.title)
-driver.find_element(By.XPATH, "//*[@name='q']").send_keys("Hellow appium....")
-time.sleep(20)
+drop_down = Select(driver.find_element(By.CSS_SELECTOR, "#searchLanguage"))
+drop_down.select_by_value("hi")
+time.sleep(5)
+driver.quit()
+appium_service.stop()
